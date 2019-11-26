33c3dff52bbcbd97a19f77cd2675f53ae6e7bee5
AJ Suchovsky <ajsuchov@gmail.com>
2019-11-26 16:02:59 -0500

    Works but not efficient enough.
    This works and writes to the excel file but it is not nearly as fast as I want it to be.

--- Processing reading.py
+++ Processing reading.py
@@ -1,164 +1,42 @@
-# import load_workbook
-from openpyxl import load_workbook
+from openpyxl import load_workbook #Import workbook
+from openpyxl import Workbook #Create and import workbook
 import multiprocessing
 from multiprocessing import Pool
 import time
 from copy import copy, deepcopy
 from operator import itemgetter
-# set file path
+
+###########################################################################################
+############# Retrieve excel file to read
+
+# set file path to retrieve file
 #filepath="C:\\Users\\AJ Suchovsky\\Desktop\\Multiprocess examples\\Absence_Roster.xlsx"
 filepath="C:\\Users\\suchovaj\\Desktop\\Parallel Python\\Absence_Roster.xlsx"
+
 # load demo.xlsx 
 wb=load_workbook(filepath,data_only = True)
+
 # select demo.xlsx
 sheet=wb.active
+
 # get max row count
 max_row=sheet.max_row
+
 # get max column count
 max_column=sheet.max_column
-    m = []
-    if k[2] == 4:
-        m = deepcopy(k)
-        list_ms4.extend(m)
-    return list_ms4
 
-def remove_empty_brackets(testList):
-    return testList
 
-def sorted_list(list_sent):
-    list_sent = sorted(list_sent, key = itemgetter(0))
-    return list_sent
+############################################################################################
+########### Appending another excel sheet with sorted values ###############################
+######## This is to open that new workbook which will be used latter #######################
 
-if __name__ == '__main__':
-    pext4 = multiprocessing.Process(list.extend(data4))
+filepath2="C:\\Users\\suchovaj\\Desktop\\Parallel Python\\Sorted_Roster_Blank.xlsx"
 
-    pext1.start()
-    pext2.start()
-    pext3.start()
-    pext4.start()
+wb2=load_workbook(filepath2)
 
-    p5 = Pool(processes=10)
-    p6 = Pool(processes=10)
-    p7 = Pool(processes=10)
-    p8 = Pool(processes=10)
+sheet2=wb2.active
 
-    ms1 = p5.map(send_ms1_list,list) 
-    ms2 = p6.map(send_ms2_list,list)
-    ms3 = p7.map(send_ms3_list,list)
-    ms4 = p8.map(send_ms4_list,list)
-
-    p5.close()
-    p6.close()
-    p7.close()
-    p8.close()
-
-###################################################################################
-    p9 = Pool(processes=10)
-    p10 = Pool(processes=10)
-    p11 = Pool(processes=10)
-    p12 = Pool(processes=10)
-
-    #make parallel
-    list_for_1 = p9.map(remove_empty_brackets, [x for x in ms1 if x != []])
-    list_for_2 = p10.map(remove_empty_brackets, [x for x in ms2 if x != []])
-    list_for_3 = p11.map(remove_empty_brackets, [x for x in ms3 if x != []])
-    list_for_4 = p12.map(remove_empty_brackets, [x for x in ms4 if x != []])
-
-    p9.close()
-    p10.close()
-    p11.close()
-    p12.close()
-
-    sorted_list_for_1 = multiprocessing.Process(sorted_list,args=(list_for_1,))
-    sorted_list_for_2 = multiprocessing.Process(sorted_list,args=(list_for_2,))
-    sorted_list_for_3 = multiprocessing.Process(sorted_list,args=(list_for_3,))
-    sorted_list_for_4 = multiprocessing.Process(sorted_list,args=(list_for_4,))
-
-    sorted_list_for_1.start()
-    sorted_list_for_2.start()
-    sorted_list_for_3.start()
-    sorted_list_for_4.start()
-    
-    print(sorted_list_for_1)
-    print(sorted_list_for_2)
-    print(sorted_list_for_3)
-    print(sorted_list_for_4)
-    p1 = Pool(processes=10)
-    p2 = Pool(processes=10)
-    p3 = Pool(processes=10)
-    p4 = Pool(processes=10)
-
-    data1 = p1.map(print_read1, [i for i in range(2,int(max_row/4))])
-    data2 = p2.map(print_read1, [i for i in range(int(max_row/4),int(max_row/4)*2)])
-    data3 = p3.map(print_read1, [i for i in range(int(max_row/4)*2,int(max_row/4)*3)])
-    data4 = p4.map(print_read1, [i for i in range(int(max_row/4)*3,max_row+1)])
-
-    p1.close()
-    p2.close()
-    p3.close()
-    p4.close()
-
-    
-    #See if I can make this happen in parallel as well.
-    pext1 = multiprocessing.Process(list.extend(data1))
-    pext2 = multiprocessing.Process(list.extend(data2))
-    pext3 = multiprocessing.Process(list.extend(data3))
-    pext4 = multiprocessing.Process(list.extend(data4))
-
-    pext1.start()
-    pext2.start()
-    pext3.start()
-    pext4.start()
-
-    p5 = Pool(processes=10)
-    p6 = Pool(processes=10)
-    p7 = Pool(processes=10)
-    p8 = Pool(processes=10)
-
-    ms1 = p5.map(send_ms1_list,list) 
-    ms2 = p6.map(send_ms2_list,list)
-    ms3 = p7.map(send_ms3_list,list)
-    ms4 = p8.map(send_ms4_list,list)
-
-    p5.close()
-    p6.close()
-    p7.close()
-    p8.close()
-
-###################################################################################
-    p9 = Pool(processes=10)
-    p10 = Pool(processes=10)
-    p11 = Pool(processes=10)
-    p12 = Pool(processes=10)
-
-    #make parallel
-    list_for_1 = p9.map(remove_empty_brackets, [x for x in ms1 if x != []])
-    list_for_2 = p10.map(remove_empty_brackets, [x for x in ms2 if x != []])
-    list_for_3 = p11.map(remove_empty_brackets, [x for x in ms3 if x != []])
-    list_for_4 = p12.map(remove_empty_brackets, [x for x in ms4 if x != []])
-
-    p9.close()
-    p10.close()
-    p11.close()
-    p12.close()
-
-    sorted_list_for_1 = multiprocessing.Process(sorted_list,args=(list_for_1,))
-    sorted_list_for_2 = multiprocessing.Process(sorted_list,args=(list_for_2,))
-    sorted_list_for_3 = multiprocessing.Process(sorted_list,args=(list_for_3,))
-    sorted_list_for_4 = multiprocessing.Process(sorted_list,args=(list_for_4,))
-
-    sorted_list_for_1.start()
-    sorted_list_for_2.start()
-    sorted_list_for_3.start()
-    sorted_list_for_4.start()
-    
-    print(sorted_list_for_1)
-    print(sorted_list_for_2)
-    print(sorted_list_for_3)
-    print(sorted_list_for_4)
-# iterate over all cells 
-# iterate over all rows
-
+###########################################################################################
 
 list = [] #go back an name this main list
 global list_for_1
@@ -173,8 +51,6 @@ list_for_4 = []
 
 def print_read1(k):
     alist = []
-    #for i in range(1,int(max_row/4)):
-     
     # iterate over all columns
     for j in range(1,max_column+1):
           # get particular cell value    
@@ -220,64 +96,12 @@ def send_ms4_list(k):
 def remove_empty_brackets(testList):
     return testList
 
-def sorted_list(list_sent):
-    list_sent = sorted(list_sent, key = itemgetter(0))
-    return list_sent
+def sort_list(testList):
+    return testList
+
+#############################################################################################################
 
 if __name__ == '__main__':
-    pext4 = multiprocessing.Process(list.extend(data4))
-
-    pext1.start()
-    pext2.start()
-    pext3.start()
-    pext4.start()
-
-    p5 = Pool(processes=10)
-    p6 = Pool(processes=10)
-    p7 = Pool(processes=10)
-    p8 = Pool(processes=10)
-
-    ms1 = p5.map(send_ms1_list,list) 
-    ms2 = p6.map(send_ms2_list,list)
-    ms3 = p7.map(send_ms3_list,list)
-    ms4 = p8.map(send_ms4_list,list)
-
-    p5.close()
-    p6.close()
-    p7.close()
-    p8.close()
-
-###################################################################################
-    p9 = Pool(processes=10)
-    p10 = Pool(processes=10)
-    p11 = Pool(processes=10)
-    p12 = Pool(processes=10)
-
-    #make parallel
-    list_for_1 = p9.map(remove_empty_brackets, [x for x in ms1 if x != []])
-    list_for_2 = p10.map(remove_empty_brackets, [x for x in ms2 if x != []])
-    list_for_3 = p11.map(remove_empty_brackets, [x for x in ms3 if x != []])
-    list_for_4 = p12.map(remove_empty_brackets, [x for x in ms4 if x != []])
-
-    p9.close()
-    p10.close()
-    p11.close()
-    p12.close()
-
-    sorted_list_for_1 = multiprocessing.Process(sorted_list,args=(list_for_1,))
-    sorted_list_for_2 = multiprocessing.Process(sorted_list,args=(list_for_2,))
-    sorted_list_for_3 = multiprocessing.Process(sorted_list,args=(list_for_3,))
-    sorted_list_for_4 = multiprocessing.Process(sorted_list,args=(list_for_4,))
-
-    sorted_list_for_1.start()
-    sorted_list_for_2.start()
-    sorted_list_for_3.start()
-    sorted_list_for_4.start()
-    
-    print(sorted_list_for_1)
-    print(sorted_list_for_2)
-    print(sorted_list_for_3)
-    print(sorted_list_for_4)
     p1 = Pool(processes=10)
     p2 = Pool(processes=10)
     p3 = Pool(processes=10)
@@ -337,17 +161,50 @@ if __name__ == '__main__':
     p11.close()
     p12.close()
 
-    sorted_list_for_1 = multiprocessing.Process(sorted_list,args=(list_for_1,))
-    sorted_list_for_2 = multiprocessing.Process(sorted_list,args=(list_for_2,))
-    sorted_list_for_3 = multiprocessing.Process(sorted_list,args=(list_for_3,))
-    sorted_list_for_4 = multiprocessing.Process(sorted_list,args=(list_for_4,))
+#####################################################################################
+    p13 = Pool(processes=10)
+    p14 = Pool(processes=10)
+    p15 = Pool(processes=10)
+    p16 = Pool(processes=10)
 
-    sorted_list_for_1.start()
-    sorted_list_for_2.start()
-    sorted_list_for_3.start()
-    sorted_list_for_4.start()
+    list_for_1 = p13.map(sort_list, [sorted(list_for_1, key=itemgetter(0))])
+    list_for_2 = p14.map(sort_list, [sorted(list_for_2, key=itemgetter(0))])
+    list_for_3 = p15.map(sort_list, [sorted(list_for_3, key=itemgetter(0))])
+    list_for_4 = p16.map(sort_list, [sorted(list_for_4, key=itemgetter(0))])
     
-    print(sorted_list_for_1)
-    print(sorted_list_for_2)
-    print(sorted_list_for_3)
-    print(sorted_list_for_4)
+    p13.close()
+    p14.close()
+    p15.close()
+    p16.close()
+
+####################################################################################
+
+    mainlist = []
+
+    for i in list_for_1:
+        mainlist.extend(i)
+
+    for i in list_for_2:
+        mainlist.extend(i)
+
+    for i in list_for_3:
+        mainlist.extend(i)
+
+    for i in list_for_4:
+        mainlist.extend(i)
+
+##################################################################################
+    header = ['Last Name', 'First Name', 'MS Level', 'Absences']
+    mainlist.insert(0,header)
+    
+    for cadet in mainlist:
+        sheet2.append(cadet)
+
+    wb2.save(filepath2)
+
+
+    
+
+
+
+
